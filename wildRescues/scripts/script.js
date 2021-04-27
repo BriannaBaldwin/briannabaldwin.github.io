3fdd0a7dbbf675d6a2e71122ccec4741bfe2ce62
Brianna Baldwin <73656451+BriannaBaldwin@users.noreply.github.com>
2021-04-27 16:01:40 -0600

    Delete wildRescues directory

--- wildRescues/scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*
-    Student Name: Brianna Baldwin
-    File Name: script.js
-    Date: December 02, 2020
-*/
-//jQuery for hero image to consume the header window space
-    $(document).ready(function(){
-    $('.hero').height($(window).height());
-    });
